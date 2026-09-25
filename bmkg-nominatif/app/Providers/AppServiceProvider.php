b8634bd5f3be257cc1a3225290f981a8629bf020
Your Name <you@example.com>
2026-09-25 18:49:14 +0700

    Memaksa penggunaan HTTPS saat di Production

--- bmkg-nominatif/app/Providers/AppServiceProvider.php
+++ bmkg-nominatif/app/Providers/AppServiceProvider.php
@@ -24,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         // Use Tailwind pagination views
         Paginator::useTailwind();
 
